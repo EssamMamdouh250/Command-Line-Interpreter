f3581c72e66491216c7566ca9da39053363cb5a4
EssamMamdouh250 <162726764+EssamMamdouh250@users.noreply.github.com>
2025-10-12 01:11:02 +0300

    Update Terminal.java

--- Terminal.java
+++ Terminal.java
@@ -1,16 +1,146 @@
-import java.nio.file.Path;
-import java.util.List;
+import java.nio.file.*;
+import java.util.*;
 
 public class Terminal {
-    private Path currentPath;
 
-    public Terminal() {
-        // TODO: Initialize current path here
+    public void execute(String command, String[] args) {
+        try {
+            switch (command) {
+                case "pwd":
+                    pwd();
+                    break;
+
+                case "cd":
+                    cd(args);
+                    break;
+
+                case "ls":
+                    ls();
+                    break;
+
+                case "mkdir":
+                    mkdir(args);
+                    break;
+
+                case "rmdir":
+                    rmdir(args);
+                    break;
+
+                case "touch":
+                    touch(args);
+                    break;
+
+                case "rm":
+                    rm(args);
+                    break;
+
+                case "cp":
+                    cp(args);
+                    break;
+
+                case "cp-r":
+                case "cp -r":
+                    cp_r(args);
+                    break;
+
+                case "cat":
+                    cat(args);
+                    break;
+
+                case "wc":
+                    wc(args);
+                    break;
+
+                case ">":
+                    // TODO
+                    break;
+
+                case ">>":
+                    // TODO
+                    break;
+
+                case "zip":
+                    zip(args);
+                    break;
+
+                case "unzip":
+                    unzip(args);
+                    break;
+
+                case "exit":
+                    System.out.println("Exiting Program CLI, Goodbye...");
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Error: Unknown command '" + command + "'");
+                    break;
+            }
+        } catch (Exception e) {
+            System.out.println("Error while executing command: " + e.getMessage());
+        }
     }
 
-    public void execute(Parser parser) {
-        // TODO: Implement command execution logic here
+    // ========== Commands skeleton ==========
+
+    public String pwd() {
+        // TODO
+        return "pwd";
     }
 
-    // TODO: Add command methods (pwd, cd, ls, mkdir, rmdir, touch, cp, etc.)
+    public void cd(String[] args) {
+        // TODO
+    }
+
+    public void ls() {
+        // TODO
+    }
+
+    public void mkdir(String[] args) {
+        // TODO
+    }
+
+    public void rmdir(String[] args) {
+        // TODO
+    }
+
+    public void touch(String[] args) {
+        // TODO
+    }
+
+    public void rm(String[] args) {
+        // TODO
+    }
+
+    public void cp(String[] args) {
+        // TODO
+    }
+
+    public void cp_r(String[] args) {
+        // TODO
+    }
+
+    public void cat(String[] args) {
+        // TODO
+    }
+
+    public void wc(String[] args) {
+        // TODO
+    }
+
+    public void redirectOutput(String command, String fileName) {
+        // TODO (for > redirection)
+    }
+
+    public void appendOutput(String command, String fileName) {
+        // TODO (for >> redirection)
+    }
+
+    public void zip(String[] args) {
+        // TODO
+    }
+
+    public void unzip(String[] args) {
+        // TODO
+    }
 }
