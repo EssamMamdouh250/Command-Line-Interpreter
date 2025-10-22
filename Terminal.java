f515f4dc45b67de3f72370291aafc85ba2502b47
Mohamedesam122 <162317445+Mohamedesam122@users.noreply.github.com>
2025-10-22 17:01:48 +0300

    Update Terminal.java

--- Terminal.java
+++ Terminal.java
@@ -1,13 +1,7 @@
 package com.mycompany.terminal;
 
-import java.util.Scanner;
-import java.util.Arrays;
-import java.io.File;
-import java.io.IOException;
-import java.io.FileWriter;
-import java.io.PrintWriter;
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
+import java.util.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
@@ -16,9 +10,6 @@ import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 import java.util.zip.ZipInputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.util.Comparator;
 
 class Command {
     String name;
@@ -29,8 +20,9 @@ class Command {
         this.args = args;
     }
 
-    public void execute() {
+    public String[] execute() {
         System.out.println("Command not implemented: " + name);
+        return null;
     }
 }
 
@@ -56,6 +48,7 @@ class Parser {
 
 public class Terminal {
     Parser p = new Parser();
+    boolean redirect = false;
     // Helper function to check if it is a file or no
     private static boolean checkFile(Path path, String label) {
         if (!Files.exists(path)) {
@@ -83,16 +76,28 @@ public class Terminal {
     }
 
 
-    public class PwdCommand extends Command {
-        public PwdCommand(String[] args) {
-            super("pwd", args);
+public class PwdCommand extends Command {
+    public PwdCommand(String[] args) {
+        super("pwd", args);
+    }
+
+    @Override
+    public String[] execute() {
+        
+        if (args != null && args.length > 0) {
+            System.out.println("pwd takes no arguments");
+            return null;
         }
 
-        @Override
-        public void execute() {
-            System.out.println(System.getProperty("user.dir"));
-        }
+        
+        String currentDir = System.getProperty("user.dir");
+        System.out.println(currentDir);
+
+        String[] redirectOutput = {currentDir};
+        return redirectOutput;
     }
+}
+
 
     public class CpCommand extends Command {
         public CpCommand(String[] args) {
@@ -100,16 +105,16 @@ public class Terminal {
         }
 
         @Override
-        public void execute() {
+        public String[] execute() {
             if (args == null || args.length != 2) {
                 System.out.println("cp <source> <destination>");
-                return;
+                return null;
             }
 
             Path src = Path.of(args[0]);
             Path dest = Path.of(args[1]);
             if (!checkFile(src, "Source") ) {
-                return;
+                return null;
             }
 
 
@@ -117,7 +122,7 @@ public class Terminal {
                 try {
                     if (Files.exists(dest) && Files.isSameFile(src, dest)) {
                         System.out.println("source and destination are the same file.");
-                        return;
+                        return null;
                     }
                 } catch (IOException ignored) {
                 }
@@ -129,6 +134,7 @@ public class Terminal {
             } catch (Exception e) {
                 System.out.println("error: " + e.getMessage());
             }
+            return null;
         }
     }
 
@@ -138,17 +144,17 @@ public class Terminal {
         }
 
         @Override
-        public void execute() {
+        public String[] execute() {
             if (args == null || args.length != 3) {
                 System.out.println("cp -r <sourceDir> <destinationDir>");
-                return;
+                return null;
             }
 
             Path src = Path.of(args[1]);
             Path dest = Path.of(args[2]);
 
             if (!checkDirectory(src, "Source") || !checkDirectory(dest, "Destination")) {
-                return;
+                return null;
             }
             // Check if they r inside each other  and stop that
             try {
@@ -156,7 +162,7 @@ public class Terminal {
                 Path Dest_Real = dest.toRealPath();
                 if (Dest_Real.startsWith(Src_Real)) {
                     System.out.println("Destination is inside source ");
-                    return;
+                    return null;
                 }
             } catch (IOException e) {
 
@@ -164,7 +170,7 @@ public class Terminal {
                 Path Dest_Abs = dest.toAbsolutePath().normalize();
                 if (Dest_Abs.startsWith(Src_Abs)) {
                     System.out.println("Destination is inside source ");
-                    return;
+                    return null;
                 }
             }
 
@@ -192,6 +198,7 @@ public class Terminal {
             } catch (IOException e) {
                 System.out.println("I/O error while walking source: " + e.getMessage());
             }
+            return null;
         }
     }
 
@@ -201,10 +208,10 @@ public class Terminal {
         }
 
         @Override
-        public void execute() {
+        public String[] execute() {
             if (args == null || args.length != 1) {
                 System.out.println("rm <file>");
-                return;
+                return null;
             }
             Path target = Path.of(args[0]);
             try {
@@ -212,14 +219,14 @@ public class Terminal {
 
 
                 if (!checkFile(target, "Target")) {
-                    return;
+                    return null;
                 }
                 Files.delete(targetAbs);
                 System.out.println("Removed " + target.getFileName());
             } catch (IOException e) {
                 System.out.println("rm: I/O error: " + e.getMessage());
             }
-
+            return null;
         }
     }
 
@@ -229,7 +236,7 @@ public class Terminal {
         }
 
         @Override
-        public void execute() {
+        public String[] execute() {
             try {
                 if (args.length == 0) {
                     System.setProperty("user.dir", System.getProperty("user.home"));
@@ -256,50 +263,154 @@ public class Terminal {
             } catch (Exception e) {
                 System.out.println("cd error: " + e.getMessage());
             }
+            return null;
         }
     }
-        public class LsCommand extends Command {
-    public LsCommand(String[] args) {
-        super("ls", args);
-    }
+    public class LsCommand extends Command {
+        public LsCommand(String[] args) {
+            super("ls", args);
+        }
 
-    @Override
-    public void execute() {
-        try {
-            int len = (args == null) ? 0 : args.length;
+        @Override
+        public String[] execute() {
+            List<String> redirectOutput = new ArrayList<>();
+            try {
+                int len = (args == null) ? 0 : args.length;
 
-            if (len > 0) {
-                System.out.println("ls takes no arguments");
-                return;
+                if (redirect){
+                    if (args.length == 1){
+                        System.out.println("no file provided to redirect to");
+                        redirect = false;
+                        return null;
+                    }
+                }
+                else if (len > 0){
+                    System.out.println("ls takes no arguments");
+                    return null;
+                }
+
+                File currentDir = new File(System.getProperty("user.dir"));
+                File[] files = currentDir.listFiles();
+
+                if (files == null) {
+                    System.out.println("Error reading directory contents");
+                    return null;
+                }
+
+                if (files.length == 0) {
+                    System.out.println("(empty directory)");
+                    String[] redirectOutputempty = {""};
+                    return redirectOutputempty;
+                }
+
+
+                 Arrays.sort(files, Comparator.comparing(File::getName));
+
+                for (File f : files) {
+                    System.out.println(f.getName());
+                    redirectOutput.add(f.getName());
+                }
+                return redirectOutput.toArray(new String[redirectOutput.size()]);
+
+            } catch (Exception e) {
+                System.out.println("ls error: " + e.getMessage());
             }
-
-            File currentDir = new File(System.getProperty("user.dir"));
-            File[] files = currentDir.listFiles();
-
-            if (files == null) {
-                System.out.println("Error reading directory contents");
-                return;
-            }
-
-            if (files.length == 0) {
-                System.out.println("(empty directory)");
-                return;
-            }
-
-            
-             Arrays.sort(files, Comparator.comparing(File::getName));
-
-            for (File f : files) {
-                System.out.println(f.getName());
-            }
-
-        } catch (Exception e) {
-            System.out.println("ls error: " + e.getMessage());
+            return null;
         }
     }
-}
 
-    
+    public class CatCommand extends Command {
+        public CatCommand(String[] args) {
+            super("cat", args);
+        }
+
+        List<String> redirectOutput = new ArrayList<>();
+
+        //Helper function to read a file
+        public void readFile(String file){
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                String line;
+                while((line = reader.readLine()) != null){
+                    System.out.println(line);
+                    redirectOutput.add(line);
+                }
+                reader.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public String[] execute() {
+            int argsNum = 0;
+            if (redirect){
+                for (int i = 0; i < args.length; i++){
+                    if (args[i].equals(">")){
+                        break;
+                    }
+                    argsNum++;
+                }
+            }
+            else {argsNum = args.length;}
+            if (argsNum > 1){
+                if (checkFile(Path.of(args[0]), "source 1")){
+                    readFile(args[0]);
+                }
+                if (checkFile(Path.of(args[1]), "source 2")){
+                    readFile(args[1]);
+                }
+                return redirectOutput.toArray(new String[redirectOutput.size()]);
+            } else if (argsNum == 1) {
+                if (checkFile(Path.of(args[0]), "source 1")){
+                    readFile(args[0]);
+                    return redirectOutput.toArray(new String[redirectOutput.size()]);
+                }
+            } else {
+                System.out.println("No file path provided.");
+                redirect = false;
+                return null;
+            }
+
+            return null;
+        }
+    }
+    public class WcCommand extends Command {
+        public WcCommand(String[] args) {
+            super("wc", args);
+        }
+        @Override
+        public String[] execute() {
+            if (args.length == 0 || args == null) {
+                System.out.println("no file given.");
+                redirect = false;
+                return null;
+            }
+            if (!checkFile(Path.of(args[0]), "source 1")){
+                redirect = false;
+                return null;
+            }
+            int lines = 0, letters = 0, words = 0;
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+                String line;
+                while((line = reader.readLine()) != null){
+                    lines++;
+                    letters += line.length();
+                    words++;
+                    for (int i = 0; i < line.length(); i++){
+                        if (line.charAt(i) == ' '){words++;}
+                    }
+                }
+                reader.close();
+                System.out.printf("%d %d %d %s\n", lines, words, letters, args[0]);
+                String[] redirectOutput = {String.format("%d %d %d %s\n", lines, words, letters, args[0])};
+                return redirectOutput;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
     public class ZipCommand extends Command {
         public ZipCommand(String[] args) {
@@ -307,11 +418,11 @@ public class Terminal {
         }
 
         @Override
-        public void execute() {
+        public String[] execute() {
             try {
                 if (args.length < 2) {
                     System.out.println("zip <archive.zip> <files...>");
-                    return;
+                    return null;
                 }
 
                 boolean recursive = args[0].equals("-r");
@@ -337,6 +448,7 @@ public class Terminal {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            return null;
         }
 
         private void addFileToZip(File file, ZipOutputStream zos) throws IOException {
@@ -377,10 +489,10 @@ public class Terminal {
         }
 
         @Override
-        public void execute() {
+        public String[] execute() {
             if (args.length < 1) {
                 System.out.println("Usage: unzip <archive.zip> [-d destination]");
-                return;
+                return null;
             }
 
             String zipFile = args[0];
@@ -415,6 +527,7 @@ public class Terminal {
             } catch (IOException e) {
                 System.out.println("Error while unzipping: " + e.getMessage());
             }
+            return null;
         }
     }
 
@@ -422,6 +535,17 @@ public class Terminal {
     public void chooseCommandAction() {
         String cmdName = p.getCommandName();
         String[] args = p.getArgs();
+        String[] redirectOutput;
+        int redirectIndex = 0;
+        redirect = false;
+
+        // check if there is a redirect command
+        for (int i = 0; i < args.length; i++){
+            if (args[i].equals(">")){
+                redirect = true;
+                redirectIndex = i;
+            }
+        }
 
         Command cmd;
 
@@ -452,12 +576,47 @@ public class Terminal {
             case "unzip":
                 cmd = new UnzipCommand(args);
                 break;
+            case "cat":
+                cmd = new CatCommand(args);
+                break;
+            case "wc":
+                cmd = new WcCommand(args);
+                break;
             default:
                 cmd = new Command(cmdName, args);
                 break;
         }
 
-        cmd.execute();
+        // store the redirect output
+        redirectOutput = cmd.execute();
+        // check if there is a redirect and if so redirect to file
+        if (redirect){
+            if (redirectOutput.length == 0 || redirectOutput == null){
+                System.out.println("no output to redirect");
+            }
+            else if (args.length == redirectIndex + 1){
+                System.out.println("no file provided to redirect to");
+                return;
+            }
+            else {
+                try {
+                    BufferedWriter writer = new BufferedWriter(new FileWriter(args[redirectIndex + 1]));
+                    if (redirectOutput.length == 1){
+                        writer.write(redirectOutput[0]);
+                    }
+                    else {
+                        writer.write(redirectOutput[0]);
+                        for (int i = 1; i < redirectOutput.length; i++){
+                            writer.write("\n" + redirectOutput[i]);
+                        }
+                    }
+                    writer.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+        }
     }
 
     public static void main(String[] args) {
